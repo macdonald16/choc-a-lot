060bc5d049b0c896ad9755dd10388d4c1eb737e5
macdonald16 <gomomac16@gmail.com>
2023-11-28 15:49:59 +0200

    lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,32 @@
 import './App.css'
 import {  Route, Routes, BrowserRouter } from 'react-router-dom'
+import { lazy, Suspense } from 'react';
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Contact from './pages/Contact'
-import Product from './pages/Product'
-import Main from './pages/Main'
+const Main = lazy(() => import('./pages/Main'));
+const Product = lazy(() => import('./pages/Product'));
+const Contact = lazy(() => import('./pages/Contact'));
+
 
 function App() {
   return (
     <BrowserRouter>
       <div>        
-      <Header />
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer/>
+        <Header />
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/product" element={<Product />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Suspense>
+
+        <Footer/>
       </div>
     </BrowserRouter>
-  )
+  );
 }
+
 
 export default App
